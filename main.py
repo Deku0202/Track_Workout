394316981c0b083e3d41b5e4c4493d5698ee2bdc
Deku <linmyathu195@gmail.com>
2025-06-22 10:04:07 -0500

    login and session

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import sqlite3
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
 app = Flask(__name__)
+app.secret_key = "deku"
 
 # Configure SQLite database
 
@@ -21,11 +22,42 @@ db = conn.cursor()
 # the associated function.
 @app.route('/')
 def main():
-    return render_template("home.html")
+    if len(session) != 1:
+        return redirect("/signup")
 
-@app.route('/login')
+    else:
+        return render_template("home.html")
+
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    
+    #User will reach to login via post method
+    if request.method == "POST":
+        #get form data
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        #return back to samepage if there is no input 
+        if not username or not password:
+            return render_template("login.html")
+        
+        #Ensure both username and password are correct 
+        info = db.execute("SELECT * FROM users WHERE username = ?", (username,)).fetchone()
+
+        #Checking if user exist and password is correct 
+        if info is None or not check_password_hash(info[2], password):
+            return render_template("login.html")
+        
+        #Remember User Sessoins
+        session["user_id"] = info[0]
+        
+        #Redirect to home page after login
+        return redirect("/")
+    else:
+        if "user" in session:
+            return redirect("/")
+        
+        return render_template("login.html")
 
 @app.route('/register', methods=["GET","POST"])
 def register():
