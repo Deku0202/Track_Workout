2a2d9a69f106e4c6c21b62b98eab34b7dbcd17aa
Deku <linmyathu195@gmail.com>
2025-06-22 01:35:18 -0500

    signup backend

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_login import UserMixin
+import sqlite3
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
 app = Flask(__name__)
 
+# Configure SQLite database
+
+# Connect db
+conn = sqlite3.connect("database.db", check_same_thread=False)
+db = conn.cursor()
+
+
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
 # the associated function.
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return 'Hello World'
+def main():
+    return render_template("home.html")
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/regisiter', methods=["GET","POST"])
+def regisiter():
+    if request.method == "POST":
+        
+        #get form data
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        #return back to samepage if there is no input 
+        if not username or not password:
+            return render_template("signup.html")
+
+        # #check no same name
+        name = db.execute("SELECT username FROM users WHERE username = ?", (username,)).fetchone()
+        if name is not None:
+            return render_template("signup.html")
+        
+        #generate password in hash 
+        
+        
+        #insert the user_data into the database 
+        db.execute("INSERT INTO users (username, password) VALUES(?, ?)", (username, password))
+        conn.commit()
+        
+        return redirect("/login")
+        
+    return render_template("signup.html")
+    
 # main driver function
 if __name__ == '__main__':
 
